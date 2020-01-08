371b74a13671d0d564c737ab37e8890fef0273b9
JayTheriault <59572377+JayTheriault@users.noreply.github.com>
2020-01-07 22:04:37 -0500

    Add files via upload

---
+++ advanced_strategy_bot_jay_v1/player.py
@@ -0,0 +1,189 @@
+'''
+Simple example pokerbot, written in Python.
+'''
+from skeleton.actions import FoldAction, CallAction, CheckAction, RaiseAction
+from skeleton.states import GameState, TerminalState, RoundState
+from skeleton.states import NUM_ROUNDS, STARTING_STACK, BIG_BLIND, SMALL_BLIND
+from skeleton.bot import Bot
+from skeleton.runner import parse_args, run_bot
+
+
+class Player(Bot):
+    '''
+    A pokerbot.
+    '''
+
+    def __init__(self):
+        '''
+        Called when a new game starts. Called exactly once.
+
+        Arguments:
+        Nothing.
+
+        Returns:
+        Nothing.
+        '''
+        
+        values = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
+
+        card_strength = {v:values.index(v) for v in values}
+        #creates dictionary to see what cards win
+
+        #create a directed graph with 13 nodes and if a certain card beats another add a node
+
+    def handle_new_round(self, game_state, round_state, active):
+        '''
+        Called when a new round starts. Called NUM_ROUNDS times.
+
+        Arguments:
+        game_state: the GameState object.
+        round_state: the RoundState object.
+        active: your player's index.
+
+        Returns:
+        Nothing.
+        '''
+        my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
+        game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
+        round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
+        my_cards = round_state.hands[active]  # your cards
+        big_blind = bool(active)  # True if you are the big blind
+        pass
+
+    def handle_round_over(self, game_state, terminal_state, active):
+        '''
+        Called when a round ends. Called NUM_ROUNDS times.
+
+        Arguments:
+        game_state: the GameState object.
+        terminal_state: the TerminalState object.
+        active: your player's index.
+
+        Returns:
+        Nothing.
+        '''
+        my_delta = terminal_state.deltas[active]  # your bankroll change from this round
+        previous_state = terminal_state.previous_state  # RoundState before payoffs
+        street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
+        my_cards = previous_state.hands[active]  # your cards
+        opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
+
+        self.updateCardStrength(my_delta, previous_state, street, my_cards, opp_cards)
+        
+
+    def get_action(self, game_state, round_state, active):
+        '''
+        Where the magic happens - your code should implement this function.
+        Called any time the engine needs an action from your bot.
+
+        Arguments:
+        game_state: the GameState object.
+        round_state: the RoundState object.
+        active: your player's index.
+
+        Returns:
+        Your action.
+        '''
+        legal_actions = round_state.legal_actions()  # the actions you are allowed to take
+        street = round_state.street  # 0, 3, 4, or 5 representing pre-flop, flop, river, or turn respectively
+        my_cards = round_state.hands[active]  # your cards
+        board_cards = round_state.deck[:street]  # the board cards
+        my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
+        opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
+        my_stack = round_state.stacks[active]  # the number of chips you have remaining
+        opp_stack = round_state.stacks[1-active]  # the number of chips your opponent has remaining
+        continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
+        my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
+        opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
+
+        if RaiseAction in legal_actions:
+            min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
+            min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
+            max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
+            
+        if CheckAction in legal_actions:  # check-call
+            return CheckAction()
+        return CallAction()
+
+
+    def checkFlush(self, cards, board_cards):
+        '''
+        Returns True if there is a flush, False otherwise
+
+        '''
+        clubs = diamonds = hearts = spades = 0
+
+        for card in range(len(cards)):
+            if cards[card][1] == 'c':
+                clubs += 1
+            elif cards[card][1] == 'd':
+                diamonds += 1
+            elif cards[card][1] == 'h':
+                hearts += 1
+            else:
+                spades += 1
+
+        for card in range(len(board_cards)):
+            if board_cards[card][1] == 'c':
+                clubs += 1
+            elif board_cards[card][1] == 'd':
+                diamonds += 1
+            elif board_cards[card][1] == 'h':
+                hearts += 1
+            else:
+                spades += 1
+
+        # print('test')
+
+        if clubs >= 5 or diamonds >= 5 or spades >= 5 or hearts >= 5:
+            return True
+        return False
+
+
+
+    def updateCardStrength(self, my_delta, previous_state, street, my_cards, opp_cards):
+        board_cards = previous_state.deck[:street]
+        
+        if self.checkFlush(my_cards, board_cards) or self.checkFlush(opp_cards, board_cards):
+            print('flush')
+            print(my_cards, opp_cards, board_cards)
+            pass
+
+        if my_delta == 0:
+            #check for same high card
+            #share high card or high pair
+
+            #check for pairs, if either of us has pair that both don't have we know there must be a straight
+
+            #check if we have the same high card, if so then do nothing
+
+            #if different high cards swap highest card with highest shared card
+            pass
+        elif my_delta >= 0:
+            #see how my hand is better than opp hand, update cards as needed
+
+            #check if both of us have pairs
+            #if so update highest card
+
+            #check if hand strength doesn't amkes sense --> implies straight
+            pass
+        elif my_delta <= 0:
+            #see how opp hand is better than my hand, update cards as needed
+            pass
+
+
+    def determineHandStrength(self, my_cards, board_cards, active):
+        big_blind = bool(active) #small blind has position advantage
+        pass
+
+    def determinePlayerRange(self, board_cards, Opp = True):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    run_bot(Player(), parse_args())
+
+
+
